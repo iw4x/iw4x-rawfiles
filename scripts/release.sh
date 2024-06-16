4255047379f7fd15e0fad00897ae04326753283b
Louvenarde <louve@louve.systems>
2024-06-16 11:18:45 +0200

    Include every IWD in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,8 @@ sudo apt update >> /dev/null && sudo apt install zip -y >> /dev/null # Ensure zi
 
 cd iw4x/
 
+# maybe this should be... a for loop instead
+
 # iw4x_00
 cd iw4x_00/
 zip -r iw4x_00.iwd *
@@ -25,6 +27,27 @@ mv iw4x_02.iwd ../
 cd ..
 rm -rf iw4x_02/
 
+# iw4x_03
+cd iw4x_03/
+zip -r iw4x_03.iwd *
+mv iw4x_03.iwd ../
+cd ..
+rm -rf iw4x_03/
+
+# iw4x_04
+cd iw4x_04/
+zip -r iw4x_04.iwd *
+mv iw4x_04.iwd ../
+cd ..
+rm -rf iw4x_04/
+
+
+# iw4x_05
+cd iw4x_05/
+zip -r iw4x_05.iwd *
+mv iw4x_05.iwd ../
+cd ..
+rm -rf iw4x_05/
 
 # Cleanup
 cd ..
