23a9e8dd9d3ab28765b65b4dbc3dec0717864f6a
mxve <68632137+mxve@users.noreply.github.com>
2024-09-30 00:55:46 +0200

    rewrite release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,64 +1,42 @@
 #!/bin/bash
 
-sudo apt update >> /dev/null && sudo apt install zip -y >> /dev/null # Ensure zip is up to date.
+# set work_dir to the parent of this scripts location
+work_dir=$(dirname `cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd`)
+iwd_files=( "iw4x_00" "iw4x_01" "iw4x_02" "iw4x_03" "iw4x_04" "iw4x_05" )
 
-cd iw4x/
+# ensure dependencies are installed
+sudo apt update >> /dev/null && sudo apt install zip curl -y >> /dev/null
 
-# maybe this should be... a for loop instead
+# create release dir
+mkdir $work_dir/release
 
-# iw4x_00
-cd iw4x_00/
-zip -r iw4x_00.iwd *
-mv iw4x_00.iwd ../
-cd ..
-rm -rf iw4x_00/
+# copy artifacts to release dir
+cp iw4x.exe $work_dir/release/
+cp iw4sp.exe $work_dir/release/
+cp -r $work_dir/zone/ $work_dir/release/
+cp -r $work_dir/iw4x/ $work_dir/release/
 
-# iw4x_01
-cd iw4x_01/
-zip -r iw4x_01.iwd *
-mv iw4x_01.iwd ../
-cd ..
-rm -rf iw4x_01/
+# zip iwd_files
+for iwd in "${iwd_files[@]}"; do
+    pushd $work_dir/release/iw4x/$iwd
+    zip -r $iwd.zip *
+    mv $iwd.zip $work_dir/release/iw4x/$iwd.iwd
+    popd
+    rm -r $work_dir/release/iw4x/$iwd
+done
 
-# iw4x_02
-cd iw4x_02/
-zip -r iw4x_02.iwd *
-mv iw4x_02.iwd ../
-cd ..
-rm -rf iw4x_02/
-
-# iw4x_03
-cd iw4x_03/
-zip -r iw4x_03.iwd *
-mv iw4x_03.iwd ../
-cd ..
-rm -rf iw4x_03/
-
-# iw4x_04
-cd iw4x_04/
-zip -r iw4x_04.iwd *
-mv iw4x_04.iwd ../
-cd ..
-rm -rf iw4x_04/
-
-
-# iw4x_05
-cd iw4x_05/
-zip -r iw4x_05.iwd *
-mv iw4x_05.iwd ../
-cd ..
-rm -rf iw4x_05/
-
-cd ..
-
-# zonebuilder-wrapper
+# add zonebuilder-wrapper
+pushd $work_dir/release/
 curl -L https://github.com/mxve/zonebuilder-wrapper/releases/latest/download/zonebuilder-i686-pc-windows-msvc.zip -o zonebuilder.zip
 unzip zonebuilder.zip
 rm zonebuilder.zip
+popd
 
-# Cleanup
-rm LICENSE
-rm README.md
-rm -rf .github
-rm -rf scripts
+# create release.zip from release dir
+pushd $work_dir/release/
 zip -r release.zip *
+mv release.zip $work_dir
+popd
+
+# cleanup
+rm -r release
